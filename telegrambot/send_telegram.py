494525b64f1ebb48f7fe34841807aaed70696505
Cay-Eric Schimanski <cayericschimanski@gmail.com>
2020-04-26 12:56:40 +0200

    add switch send mode with message read from stdin

--- telegrambot/send_telegram.py
+++ telegrambot/send_telegram.py
@@ -5,6 +5,7 @@ import logging
 import os.path
 import os
 import sys
+from select import select
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Bot
@@ -24,19 +25,31 @@ def main():
     chat_id = chat_id_file.read()
     chat_id_file.close()
 
+    # read message from source depending on given arguments
     if len(sys.argv) == 3 and sys.argv[1]=="-f":
         # read message from file, but truncate to 4000 chars
         msg_file = open(sys.argv[2], "r")
         message = msg_file.read()
         msg_file.close()
-        if len(message) > 4000:
-            message = message[:4000]+"\n\n... truncated"
-        Bot(os.environ['TELEGRAM_BOT_ID']).send_message(chat_id, message)
-    elif len(sys.argv) != 2:
-        print("Usage: send_telegram.py TEXTMESSAGE | -f MESSAGEFILE")
-        sys.exit(1)
+    elif len(sys.argv) == 2 and sys.argv[1]=="-i":
+        # read message from stdin
+        rlist, _, _ = select([sys.stdin], [], [], 3)
+        if rlist:
+            message = sys.stdin.readline()
+        else:
+            print ("No input. Abort.")
+            sys.exit(2)
+    elif len(sys.argv) == 3 and sys.argv[1]=="-t":
+        message=sys.argv[2]
     else:
-        Bot(os.environ['TELEGRAM_BOT_ID']).send_message(chat_id, sys.argv[1])
+        print("Usage: send_telegram.py -t TEXTMESSAGE | -f MESSAGEFILE | -i")
+        sys.exit(1)
+
+    if len(message) > 4000:
+        message = message[:4000]+"... truncated"
+
+    print("I'd like to send out: " + message)
+    Bot(os.environ['TELEGRAM_BOT_ID']).send_message(chat_id, message)
 
 if __name__ == '__main__':
     main()
